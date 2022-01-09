41a97e6ef0093fd87f6804cdab3b68613750f4bb
Roman <r.zharuk@ukr.net>
2022-01-09 15:31:39 +0200

    constructors

--- FractionClass.cpp
+++ FractionClass.cpp
@@ -1,2 +1,13 @@
 #include "FractionClass.h"
 
+FractionClass::FractionClass()
+{
+	numer = 1;
+	denom = 1;
+}
+
+FractionClass::FractionClass(int numer, int denom)
+{
+	this->numer = numer;
+	this->denom = denom;
+}
--- FractionClass.h
+++ FractionClass.h
@@ -4,6 +4,13 @@ using namespace std;
 
 class FractionClass
 {
+	int numer; //numerator
+	int denom; //denominator
+
+public:
+
+	FractionClass();
+	FractionClass(int numer, int denom);
 
 };
 
